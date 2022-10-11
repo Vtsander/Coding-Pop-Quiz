8a6b89747a611cc1f280a8a2447b3ac7c9ab4c8f
vtsander <nguyenveroneca@yahoo.com>
2022-10-11 15:50:45 -0700

    more updates

--- assets/script.js
+++ assets/script.js
@@ -50,24 +50,46 @@ var quizContainer = document.getElementById('quiz');
 var resultsContainer = document.getElementById('results');
 var submitButton = document.getElementById('submit');
 
+document.getElementById('timer').innerHTML =
+  01 + ":" + 11;
+startTimer();
+
+function startTimer() {
+  var presentTime = document.getElementById('timer').innerHTML;
+  var timeArray = presentTime.split(/[:]+/);
+  var m = timeArray[0];
+  var s = checkSecond((timeArray[1] - 1));
+  if(s==59){m=m-1}
+  if(m<0){
+    return
+  }
+  
+  document.getElementById('timer').innerHTML =
+    m + ":" + s;
+  console.log(m)
+  setTimeout(startTimer, 1000);
+  
+}
+
+function checkSecond(sec) {
+  if (sec < 10 && sec >= 0) {sec = "0" + sec}; // add zero in front of numbers < 10
+  if (sec < 0) {sec = "59"};
+  return sec;
+}
+
 generateQuiz(myQuestions, quizContainer, resultsContainer, submitButton);
 
 function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
-
 	function showQuestions(questions, quizContainer){
 		// we'll need a place to store the output and the answer choices
 		var output = [];
 		var answers;
-
 		// for each question...
 		for(var i=0; i<questions.length; i++){
-			
 			// first reset the list of answers
 			answers = [];
-
 			// for each available answer...
 			for(letter in questions[i].answers){
-
 				// ...add an html radio button
 				answers.push(
 					'<label>'
@@ -77,39 +99,29 @@ function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
 					+ '</label>'
 				);
 			}
-
 			// add this question and its answers to the output
 			output.push(
 				'<div class="question">' + questions[i].question + '</div>'
 				+ '<div class="answers">' + answers.join('') + '</div>'
 			);
 		}
-
 		// finally combine our output list into one string of html and put it on the page
 		quizContainer.innerHTML = output.join('');
 	}
-
-
 	function showResults(questions, quizContainer, resultsContainer){
-		
 		// gather answer containers from our quiz
 		var answerContainers = quizContainer.querySelectorAll('.answers');
-		
 		// keep track of user's answers
 		var userAnswer = '';
 		var numCorrect = 0;
-		
 		// for each question...
 		for(var i=0; i<questions.length; i++){
-
 			// find selected answer
 			userAnswer = (answerContainers[i].querySelector('input[name=question'+i+']:checked')||{}).value;
-			
 			// if answer is correct
 			if(userAnswer===questions[i].correctAnswer){
 				// add to the number of correct answers
 				numCorrect++;
-				
 				// color the answers green
 				answerContainers[i].style.color = 'lightgreen';
 			}
@@ -119,17 +131,13 @@ function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
 				answerContainers[i].style.color = 'red';
 			}
 		}
-
 		// show number of correct answers out of total
 		resultsContainer.innerHTML = numCorrect + ' out of ' + questions.length;
 	}
-
 	// show questions right away
 	showQuestions(questions, quizContainer);
-	
 	// on submit, show results
 	submitButton.onclick = function(){
 		showResults(questions, quizContainer, resultsContainer);
 	}
-
 }
